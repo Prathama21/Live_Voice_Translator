335b0a4e9e44a8e7887efa23d388230061e1c814
Prathama21 <125333471+Prathama21@users.noreply.github.com>
2023-08-18 21:26:11 +0530

    Add files via upload

---
+++ voice.py
@@ -0,0 +1,9 @@
+# from googletrans import Translator
+
+# translator = Translator()
+
+# out = translator.translate("I love you", dest="hi")
+
+# print(out.text)
+from deep_translator import GoogleTranslator
+GoogleTranslator(source='auto', target='en').translate("guten Tag")
